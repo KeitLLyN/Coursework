99fae2083cd1f8a8fb0578b9e637a365659a02b8
boris.shaporov <boris.shaporov@azati.com>
2021-05-15 13:47:41 +0300

    remove space

--- src/main/java/by/university/demo/entity/Message.java
+++ src/main/java/by/university/demo/entity/Message.java
@@ -18,13 +18,9 @@ public class Message {
 
     private String tag;
 
-
-
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
-
-
 
     public String getAuthorName() {
         return author != null ? author.getUsername() : "<none>";
