7c4f62019fdb3d678224433246ad008fa0a3e8f2
boris.shaporov <boris.shaporov@azati.com>
2021-05-15 13:38:30 +0300

    Remove password encoder

--- src/main/java/by/university/demo/config/WebSecurityConfig.java
+++ src/main/java/by/university/demo/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -42,6 +43,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(USER_SERVICE)
-                .passwordEncoder(PASSWORD_ENCODER);
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
