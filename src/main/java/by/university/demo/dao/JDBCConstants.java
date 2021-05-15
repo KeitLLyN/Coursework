a53819ba536ef42b26d189bdc4e095ab0cc628eb
boris.shaporov <boris.shaporov@azati.com>
2021-05-15 21:41:14 +0300

    Added jdbc constant

---
+++ src/main/java/by/university/demo/dao/JDBCConstants.java
@@ -0,0 +1,7 @@
+package by.university.demo.dao;
+
+public interface JDBCConstants {
+    String URL = "jdbc:mysql://127.0.0.1:3306/coursework?serverTimezone=UTC";
+    String username = "root";
+    String password = "root";
+}
