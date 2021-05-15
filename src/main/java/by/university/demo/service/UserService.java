b15ef1ea15a3c706a56a33b60514dec323a7db88
boris.shaporov <boris.shaporov@azati.com>
2021-05-15 21:37:50 +0300

    Added findAll method

--- src/main/java/by/university/demo/service/UserService.java
+++ src/main/java/by/university/demo/service/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -40,5 +41,11 @@ public class UserService implements UserDetailsService {
         USER_REPOSITORY.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        List<User> users;
+        users = USER_DAO.findAll();
+        return users;
     }
 }
