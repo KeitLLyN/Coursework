4bd137b4e52ab07a5dc98fe35831bd8d7a1dd0df
boris.shaporov <boris.shaporov@azati.com>
2021-05-15 13:38:13 +0300

    Change start controller

--- src/main/java/by/university/demo/controllers/StartController.java
+++ src/main/java/by/university/demo/controllers/StartController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StartController {
 
+
     @GetMapping("/")
     public String index() {
         return "index";
