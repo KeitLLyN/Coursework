968f77c7ce577ffddaf79bf505b8f53b5c167318
boris.shaporov <boris.shaporov@azati.com>
2021-05-16 23:52:56 +0300

    Added generate users for sql script

--- src/main/java/by/university/demo/service/UserService.java
+++ src/main/java/by/university/demo/service/UserService.java
@@ -5,14 +5,20 @@ import by.university.demo.entity.Role;
 import by.university.demo.entity.User;
 import by.university.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 @Service
 @RequiredArgsConstructor
@@ -45,5 +51,30 @@ public class UserService implements UserDetailsService {
         List<User> users;
         users = USER_DAO.findAll();
         return users;
+    }
+
+    public void generateUsers(){
+        File file = new File("src/main/resources/username.txt");
+        try(
+                Scanner scanner = new Scanner (file);
+                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/db/migration/V6__GenerateUsers_DB.sql"));
+        ){
+            while (scanner.hasNextLine()){
+                String username = scanner.nextLine();
+                String email = String.format("%s.user@gmail.com", username);
+                String decodedPassword = RandomStringUtils.randomAlphabetic(1,7);
+                String password = PASSWORD_ENCODER.encode(decodedPassword);
+
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+
+                String sql = USER_DAO.generate(user) + "\n";
+                writer.append(sql);
+            }
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
