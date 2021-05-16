cd4dc0e1c60a56a2d4dbecf4ff2e834561071092
boris.shaporov <boris.shaporov@azati.com>
2021-05-16 23:53:16 +0300

    Added password encoder as Bcrypt

--- src/main/java/by/university/demo/config/WebSecurityConfig.java
+++ src/main/java/by/university/demo/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -43,6 +42,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(USER_SERVICE)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(PASSWORD_ENCODER);
     }
 }
